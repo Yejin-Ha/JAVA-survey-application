3c04ce82d10da0b2c592a788c16bb4d420d296b5
Yejin-Ha <hyj1004mu@naver.com>
2022-05-06 11:36:23 +0900

    create JDBC template

---
+++ surveyapplication/src/surveyapplication/JdbcTemplate.java
@@ -0,0 +1,66 @@
+package surveyapplication;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcTemplate {
+	private static JdbcTemplate instance;
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String userName = "c##tester";
+	private String userPwd = "1234";
+
+	private JdbcTemplate() {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static JdbcTemplate getInstance() {
+		synchronized (JdbcTemplate.class) {
+			if (instance == null) {
+				instance = new JdbcTemplate();
+			}
+		}
+		return instance;
+	}
+
+	public Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(url, userName, userPwd);
+	}
+
+	public void close(Statement stmt) {
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void close(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void close(Connection conn) {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
