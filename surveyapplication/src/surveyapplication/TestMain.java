2e852a167f6e1b68b012beec138a52676d03c4f9
Yejin-Ha <hyj1004mu@naver.com>
2022-05-06 11:37:27 +0900

    create login, signup methods

--- surveyapplication/src/surveyapplication/TestMain.java
+++ surveyapplication/src/surveyapplication/TestMain.java
@@ -1,7 +1,23 @@
 package surveyapplication;
 
+import java.util.Scanner;
+
 public class TestMain {
 	public static void main(String[] args) {
-		System.out.println("hi");
+		User user = new User();
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("name");
+		String name = sc.nextLine();
+
+		System.out.println("pswd");
+		String pswd = sc.nextLine();
+
+		System.out.println("age");
+		int age = sc.nextInt();
+		sc.nextLine();
+		
+		System.out.println(user.login(name, pswd));
+		System.out.println(user.signUp(name, pswd, age));
 	}
 }
---
+++ surveyapplication/src/surveyapplication/User.java
@@ -0,0 +1,60 @@
+package surveyapplication;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class User {
+	private JdbcTemplate jdbcTemplate;
+
+	public boolean signUp(String name, String pswd, int age) {
+		jdbcTemplate = JdbcTemplate.getInstance();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "insert into \"INFO\" values (\"SEQ_INFO\".nextval, ?, ?, ?)";
+
+		try {
+			conn = jdbcTemplate.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, name);
+			pstmt.setString(2, pswd);
+			pstmt.setInt(3, age);
+
+			if (pstmt.executeUpdate() == 1)
+				return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			jdbcTemplate.close(pstmt);
+			jdbcTemplate.close(conn);
+		}
+		return false;
+	}
+
+	public int login(String name, String pwd) {
+		jdbcTemplate = JdbcTemplate.getInstance();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		String sql = "select \"INFO_NUMBER\" from \"INFO\" where name = ? and pswd = ?";
+		ResultSet rs = null;
+
+		try {
+			conn = jdbcTemplate.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, name);
+			pstmt.setString(2, pwd);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				return rs.getInt(1);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			jdbcTemplate.close(rs);
+			jdbcTemplate.close(pstmt);
+			jdbcTemplate.close(conn);
+		}
+		return 0;
+	}
+}
