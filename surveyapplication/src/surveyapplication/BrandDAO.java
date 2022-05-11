df02a11132697e23a7d0df831c0e3a972edfec8e
Yejin-Ha <hyj1004mu@naver.com>
2022-05-11 17:41:57 +0900

    사용자가 투표를 할 때 범위 외의 값을 입력하면 경고 후 재투표

--- surveyapplication/src/surveyapplication/BrandDAO.java
+++ surveyapplication/src/surveyapplication/BrandDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class BrandDAO {
 	private JdbcTemplate jdbcTemplate;
@@ -14,7 +16,7 @@ public class BrandDAO {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String sql = "select \"BRAND_NUMBER\", \"TITLE\" from \"BRAND\"";
+		String sql = "select \"BRAND_NUMBER\", \"TITLE\" from \"BRAND\" order by \"BRAND_NUMBER\" asc";
 
 		try {
 			conn = jdbcTemplate.getConnection();
@@ -33,10 +35,14 @@ public class BrandDAO {
 			jdbcTemplate.close(pstmt);
 			jdbcTemplate.close(conn);
 		}
-		System.out.println(length);
 	}
 
-	public void selectBrand(int infoNum, int brandNum) {
+	public boolean selectBrand(int infoNum, int brandNum) {
+		if (!brandNumber(brandNum).contains(brandNum)) {
+			System.out.println("번호를 다시 입력하세요.");
+			return false;
+		}
+		boolean result = false;
 		jdbcTemplate = JdbcTemplate.getInstance();
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -48,7 +54,7 @@ public class BrandDAO {
 			pstmt.setInt(1, infoNum);
 			pstmt.setInt(2, brandNum);
 
-			pstmt.executeUpdate();
+			result = (pstmt.executeUpdate() == 1) ? true : false;
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -56,9 +62,10 @@ public class BrandDAO {
 			jdbcTemplate.close(pstmt);
 			jdbcTemplate.close(conn);
 		}
+		return result;
 	}
 
-	public void insertBrand(int infoNumber, String brand) {
+	public boolean insertBrand(int infoNumber, String brand) {
 		jdbcTemplate = JdbcTemplate.getInstance();
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -70,15 +77,39 @@ public class BrandDAO {
 			pstmt.setString(1, brand);
 
 			pstmt.executeUpdate();
-
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			jdbcTemplate.close(pstmt);
 			jdbcTemplate.close(conn);
 		}
-		
-		selectBrand(infoNumber, length+1);
+
+		return selectBrand(infoNumber, length + 1);
 	}
 
+	private ArrayList<Integer> brandNumber(int i) {
+		jdbcTemplate = JdbcTemplate.getInstance();
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<Integer> ls = new ArrayList<>();
+		String sql = "select \"BRAND_NUMBER\" from \"BRAND\"";
+
+		try {
+			conn = jdbcTemplate.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				ls.add(rs.getInt("BRAND_NUMBER"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			jdbcTemplate.close(rs);
+			jdbcTemplate.close(pstmt);
+			jdbcTemplate.close(conn);
+		}
+		return ls;
+	}
 }
